e5e7e612e30baaca85ffc9585ed8c9df55364d8b
gelead <geleadw@gmail.com>
2024-09-09 03:52:11 -0700

    Use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,44 +1,6 @@
-const products = [
-{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 40,
-        count: 127
-    },
-    priceCents: 2095, 
-},
-{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 45,
-        count: 56
-    },
-    priceCents: 799, 
-}]
-
-products.push({
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 45,
-        count: 87
-    },
-    priceCents: 1090, 
-})
-
-
-
-
-
-
-
-
-
-let generateHtml = ''
+let productHTML = "";
 products.forEach((product) => {
-    let html = `
+  let html = `
      <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -51,7 +13,7 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
@@ -87,7 +49,7 @@ products.forEach((product) => {
             Add to Cart
           </button>
         </div> 
-        `
-        generateHtml += html
-})
-document.querySelector('.js-products-grid').innerHTML = generateHtml
+        `;
+  productHTML += html;
+});
+document.querySelector(".js-products-grid").innerHTML = productHTML;
